8e61d4b5a35ad1a7d1d137cb2c8a8ba96b75848a
gsunil1996 <gajulasunilkumar123@gmail.com>
2024-09-22 14:09:00 +0530

    conditional url added

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,13 +3,16 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	server: {
-		port: 3000,
-		proxy: {
-			"/api": {
-				target: "http://localhost:5000",
-			},
-		},
-	},
+  plugins: [react()],
+  server: {
+    port: 3000,
+    proxy: {
+      "/api": {
+        target:
+          import.meta.env.MODE === "development"
+            ? "http://localhost:5000"
+            : "https://mern-chat-app-zyhy.onrender.com",
+      },
+    },
+  },
 });
